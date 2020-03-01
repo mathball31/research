d0e6a5c68fbb7d02a6d21e3452a35f2a432b1076
mathball31 <dirk.lamb31@gmail.com>
2020-03-01 13:16:49 -0700

    added some comments

--- aig_minterm/rlrh.py
+++ aig_minterm/rlrh.py
@@ -1,5 +1,27 @@
-#TODO
 """
+This script takes an aag file representing an exact multiplier circuit, and a single minterm 
+remainder. The remainder should be the result of reducing an approximate multiplier against an exact
+spec.
+
+For each and gate in the circuit, this creates a new aag with that gate set to 0 and set to 1.
+It then (through a series of steps) generates rL and rH, made by running the two new aags against
+the approximate spec.
+
+All intermediate files will be created in a folder named after the input file and the remainder.
+eg `python3 rlrh.py mult2.aag "a(0) * (1 - b(1))"` -> `mult2_a0*-b1/`
+
+To run: 
+    python3 rlrh.py multiplier.aag "<remainder>"
+where:
+    multipler.aag: path to exact multiplier aag file
+    <remainder>: Singular style minterm
+        e.g.: "a(0)*b(1)", "number(2)^3*a(3)*(1-a(2))*b(2)*(1-b(0))"
+        use quotes to escape the '('s and ')'s
+
+NOTE: This was devolped with python version 3.7.6. It will probably work for any 3.7.x, but will 
+*NOT* work for an version less than 3.7.
+
+TODO
 x take multiplier aag, 
     x change a gate to `x 0 0`
         x run through aigtoaig to get .aig (this fixes some weird issues)
@@ -9,6 +31,8 @@ x take multiplier aag,
         x repeat above but with `x 1 1`
     x repeat above with other gates
     . store remainders
+    . reduce rL*rH by J0
+        . find J0
 
 """
 
