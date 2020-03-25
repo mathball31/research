a53aba4931a6f1220ca74e626051af8dae5f58aa
mathball31 <dirk.lamb31@gmail.com>
2020-03-24 19:06:24 -0600

    Added multiple output functionality to gen_rem

--- approx_mult/gen_rem.py
+++ approx_mult/gen_rem.py
@@ -4,11 +4,12 @@ from approx_mult_functions import *
 from pathlib import Path
 import argparse
 from itertools import chain, combinations
-from random import randrange, choice
+from random import randrange, choice, sample
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input_file_name', help='.aag representing input circuit')
 parser.add_argument('num_remainders', type=int, help='number of remainders to generate')
+parser.add_argument('num_outputs', type=int, default = 1, help='number of outputs to affect')
 
 args = parser.parse_args()
 
@@ -29,6 +30,12 @@ def concat(left, right):
     if left == "":
         return right
     return left + " * " + right
+
+#TODO comment
+def add(left, right):
+    if left == "":
+        return right
+    return left + " + " + right
 
 
 #TODO comment
@@ -57,6 +64,7 @@ for idx in range(aag.num_outputs):
     powers_of_2.append("number(2)^" + str(idx))
 
 #TODO comment
+# this only does single output changes
 def generate_all_remainders(inputs):
     powers_of_2 = []
     for idx in range(aag.num_outputs):
@@ -68,18 +76,32 @@ def generate_all_remainders(inputs):
             remainders.append(concat(power, term))
     return remainders
 
+#TODO comment
+def create_coefficient(num_outputs):
+    outputs = []
+    if num_outputs < len(powers_of_2)/2:
+        outputs = sample(powers_of_2[0:len(powers_of_2)//2], num_outputs)
+    else:
+        outputs = sample(powers_of_2, num_outputs)
+
+    coefficient = ""
+    for output in outputs:
+        coefficient = add(coefficient, output)
+    return "(" + coefficient + ")"
+
+
 """
 NOTE: total_num = num_outputs * (2^num_inputs-1)
 """
 #TODO comment
-def generate_random_remainders(inputs, num):
+def generate_random_remainders(inputs, num, num_outputs):
     count = 0
     #itrs is a safety valve, and will ensure this doesn't run forever
     itrs = 0
     remainders = set()
     while count < num and itrs < 2*num:
         #pick a random output
-        remainder = choice(powers_of_2)
+        remainder = create_coefficient(num_outputs)
         test_sum = 0
         for term in inputs:
             #pick if a term is ommited, included, or inverted
@@ -99,13 +121,16 @@ def generate_random_remainders(inputs, num):
 
 remainders = []
 max_num = aag.num_outputs * (3** aag.num_inputs - 1)
+"""
+TODO fix generate_all_remainders so it can handle multiple outputs
 if args.num_remainders > max_num:
     print("you requested more remainders than can exist for this circuit. generating all " 
             + str(max_num) + " remainders")
 if args.num_remainders >= max_num:
     remainders = generate_all_remainders(inputs)
 else:
-    remainders = generate_random_remainders(inputs, args.num_remainders)
+    """
+remainders = generate_random_remainders(inputs, args.num_remainders, args.num_outputs)
 
 """
 for rem in remainders:
@@ -119,6 +144,6 @@ temp_dir.mkdir(exist_ok = True)
 with cd(temp_dir_str):
     for count, term in enumerate(remainders):
         #TODO let user pick how often to check in
-        if count % 1 == 0:
+        if count % 10 == 0:
             print("term " + str(count) + ": " + term)
         gate_residue = reduce_rlrh(aag, term)
